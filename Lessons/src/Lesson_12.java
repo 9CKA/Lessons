c38a583d262820ff48a6beb1ece6f7d50ac808d1
9CKA <namelless1990@gmail.com>
2024-08-14 21:30:20 +0300

    redet3

--- Lessons/src/Lesson_12.java
+++ Lessons/src/Lesson_12.java
@@ -1,42 +1,39 @@
 public class Lesson_12 {
 
-    public static int sumArray(String[][] array) throws MyArraySizeException {
-        if (array.length != 4 || array[0].length != 4) {
-            throw new MyArraySizeException("Размер массива должен быть 4х4");
-        }
-        int result = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                try {
-                    result += Integer.parseInt(array[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(i + ", " + j + ": " + array[i][j] + " - это не число");
+    public class Main {
+        static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+            int sum = 0;
+            if (array.length != 4 || array[0].length != 4) {
+                throw new MyArraySizeException("Неверный размер массива");
+            }
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    try {
+                        sum += Integer.parseInt(array[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException(
+                                "Неверные данные в ячейке " + (i + 1) + ", столбец " + (j + 1));
+                    }
                 }
             }
+            return sum;
         }
-        return result;
-    }
-
-    class MyArrayException extends Exception {
-    }
     
-    class MyArrayDataException extends MyArrayException {
-        public MyArrayDataException(String message) {
-            super(message);
-        }
-    }
-    
-    class MyArraySizeException extends MyArrayException {
-        public MyArraySizeException(String message) {
-            super(message);
-        }
-    }
-
-    public class Main {
         public static void main(String[] args) {
             String[][] array = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "7", "8"},
-                {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"},
+                    {"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            };
+            try {
+                System.out.println(sumArray(array));
+            } catch (MyArraySizeException e) {
+                e.printStackTrace();
+            } catch (MyArrayDataException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
