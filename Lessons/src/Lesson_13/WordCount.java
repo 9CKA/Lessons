27753ee30b3ef4782d57fc44a65bb3f2015e87ec
9CKA <namelless1990@gmail.com>
2024-08-19 22:26:49 +0300

    restart3

--- Lessons/src/Lesson_13/WordCount.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class WordCount {
-  public static void main(String[] args) {
-    ArrayList<String> words = new ArrayList<>(Arrays.asList("дом", "кот", "собака", "кошка", "дерево", "стол", "стул", "окно", "дверь", "книга"));
-
-    Map<String, Integer> wordCounts = new HashMap<>();
